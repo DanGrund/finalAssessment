7c8ede29db05177809c8b68a0dff9d3d22e338b5
DanGrund <hello@dangrund.com>
2017-04-13 10:27:41 -0600

    set up server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('src'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+  fs.readFile(`${_dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+})
+
+if(!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`Server is running on ${app.get('port')}`);
+  })
+}
